81cf7007e9b38093106795b6f1063947644f91c1
Chris Lacaille <ChrisL108@users.noreply.github.com>
2019-04-13 17:48:21 -0500

    cleanup

--- src/MasterForm.jsx
+++ src/MasterForm.jsx
@@ -1,26 +1,26 @@
-import React, { useState, useEffect, useContext } from "react";
-import Button from "@material-ui/core/Button";
-import GeneralPage from "./Pages/GeneralPage";
-import UserInfoPage from "./Pages/UserInfoPage";
+import React, { useState, useEffect, useContext } from 'react';
+import Button from '@material-ui/core/Button';
+import GeneralPage from './Pages/GeneralPage';
+import UserInfoPage from './Pages/UserInfoPage';
 // import ProviderProps from "./Data/providerProps";
-import { ProviderPropsContext } from "./App";
+import { ProviderPropsContext } from './App';
 
 function MasterForm() {
-  // const [providerProps, setProviderProps] = useContext(ProviderPropsContext);
+    // const [providerProps, setProviderProps] = useContext(ProviderPropsContext);
 
-  return (
-    <>
-      <GeneralPage />
-      <br />
-      <UserInfoPage />
-      <br />
-      <br />
-      <br />
-      <Button variant="contained" color="primary">
-        Submit
-      </Button>
-    </>
-  );
+    return (
+        <>
+            <GeneralPage />
+            <br />
+            <UserInfoPage />
+            <br />
+            <br />
+            <br />
+            <Button variant="contained" color="primary">
+                Submit
+            </Button>
+        </>
+    );
 }
 
 export default MasterForm;
